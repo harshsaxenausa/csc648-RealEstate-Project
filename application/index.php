cdf2750368b49e71cb0e99bb5ee895cc2fbae213
Harsh Saxena <harsh.saxena.usa@gmail.com>
2020-08-20 16:22:28 -0700

    Add files via upload

---
+++ application/index.php
@@ -0,0 +1,4 @@
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/views/include/header.php' ?>
+<?php redirectTo('/views/index.php') ?>
+
+the home page is <a href="http://54.151.14.7/views/index.php">here</a>
